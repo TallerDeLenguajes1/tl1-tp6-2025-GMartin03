0852527ff19d186b19f0813d1a1e5a665f2e5873
GMartin03 <martingomez2003174@hotmail.com>
2025-05-23 18:53:01 -0300

    ej1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingese un numero");
+
+string numeroIngresado = Console.ReadLine();
+int numero;
+
+bool esNumero = int.TryParse(numeroIngresado, out numero);
+
+if (esNumero)
+{
+    if (numero > 0)
+    {
+        int invertido = 0;
+        while (numero > 0)
+        {
+            int digito = numero % 10;
+            invertido = invertido * 10 + digito;
+            numero = numero / 10;
+        }
+        Console.WriteLine("Número invertido: " + invertido);
+    }
+    else
+    {
+        Console.WriteLine("El número debe ser mayor que 0.");
+    }
+}
+else
+{
+    Console.WriteLine("Eso no es un número válido.");
+}
