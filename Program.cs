bd8b7b51303901d9c8d81c236d3598cee24aa6bc
GMartin03 <martingomez2003174@hotmail.com>
2025-05-23 20:08:30 -0300

    problemas con merge

--- Program.cs
+++ Program.cs
@@ -51,7 +51,51 @@ while (seguir)
     Console.Write("¿Desea realizar otro cálculo? (s/n): ");
     string respuesta = Console.ReadLine().ToLower();
     if (respuesta != "s")
-        {
-            seguir = false;
-        }
+    {
+        seguir = false;
+    }
 }
+
+Console.Write("Ingrese un número (puede tener coma): ");
+string numero = Console.ReadLine();
+double num;
+
+if (double.TryParse(numero, out num))
+{
+    Console.WriteLine("Valor absoluto: " + Math.Abs(num));
+    Console.WriteLine("Cuadrado: " + Math.Pow(num, 2));
+    if (num >= 0)
+    {
+        Console.WriteLine("Raíz cuadrada: " + Math.Sqrt(num));
+    }
+    else
+    {
+        Console.WriteLine("Raíz cuadrada: No se puede calcular (número negativo).");
+    }
+    Console.WriteLine("Seno: " + Math.Sin(num));
+    Console.WriteLine("Coseno: " + Math.Cos(num));
+    Console.WriteLine("Parte entera: " + Math.Truncate(num));
+}else
+    {
+        Console.WriteLine("No ingresó un número valido.");
+        return;
+    }
+
+    Console.Write("Ingrese el primer número: ");
+    string primero = Console.ReadLine();
+    Console.Write("Ingrese el segundo número: ");
+    string segundo = Console.ReadLine();
+    double numUno, numDos;
+
+    bool ok1 = double.TryParse(primero, out numUno);
+    bool ok2 = double.TryParse(segundo, out numDos);
+
+    if (ok1 && ok2)
+    {
+        Console.WriteLine("Máximo: " + Math.Max(numUno, numDos));
+        Console.WriteLine("Mínimo: " + Math.Min(numUno, numDos));
+    }
+    else
+    {
+        Console.WriteLine("Uno de los valores ingresados no es valido.");
+    }
