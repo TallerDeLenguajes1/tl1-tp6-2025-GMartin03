11a9d140afa22eedda6900584ca16e0df2196c20
GMartin03 <martingomez2003174@hotmail.com>
2025-05-23 19:07:18 -0300

    calcu1

--- Program.cs
+++ Program.cs
@@ -1,30 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ingese un numero");
+bool seguir = true;
 
-string numeroIngresado = Console.ReadLine();
-int numero;
-
-bool esNumero = int.TryParse(numeroIngresado, out numero);
-
-if (esNumero)
+while (seguir)
 {
-    if (numero > 0)
+    Console.WriteLine("Seleccione una opción (1-4):");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+
+    string opcion = Console.ReadLine();
+    int num1, num2;
+
+    Console.Write("Ingrese el primer número: ");
+    bool numero1 = int.TryParse(Console.ReadLine(), out num1);
+    bool numero2 = int.TryParse(Console.ReadLine(), out num2);
+    if (!numero1 || !numero2)
     {
-        int invertido = 0;
-        while (numero > 0)
-        {
-            int digito = numero % 10;
-            invertido = invertido * 10 + digito;
-            numero = numero / 10;
-        }
-        Console.WriteLine("Número invertido: " + invertido);
+        Console.WriteLine("Uno de los valores ingresados no es un número válido.");
     }
     else
     {
-        Console.WriteLine("El número debe ser mayor que 0.");
+        switch (opcion)
+        {
+            case "1":
+                Console.WriteLine("Resultado: " + (num1 + num2));
+                break;
+            case "2":
+                Console.WriteLine("Resultado: " + (num1 - num2));
+                break;
+            case "3":
+                Console.WriteLine("Resultado: " + (num1 * num2));
+                break;
+            case "4":
+                if (num2 != 0)
+                {
+                    Console.WriteLine("Resultado: " + (num1 / num2));
+                }
+                else
+                {
+                    Console.WriteLine("No se puede dividir por cero.");
+                }
+                break;
+            default:
+                Console.WriteLine("Opción inválida.");
+                break;
+        }
     }
-}
-else
-{
-    Console.WriteLine("Eso no es un número válido.");
+    Console.Write("¿Desea realizar otro cálculo? (s/n): ");
+    string respuesta = Console.ReadLine().ToLower();
+    if (respuesta != "s")
+        {
+            seguir = false;
+        }
 }
